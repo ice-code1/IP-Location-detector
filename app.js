e2cde9ab5416a77a0e5c394e3f49a90f7b3a6c12
ice-code1 <adebunmioluwatimilehin@gmail.com>
2024-07-01 16:58:33 +0100

    updated app.js file

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.use('/api/users', userRoutes);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('api deployed');
+  })
+  
+
 const Port = process.env.PORT || 5000;
 
 app.listen(Port, () => console.log(`Server running on port ${Port}`));
